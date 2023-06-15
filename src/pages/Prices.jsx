e0583eb932891dcea081f9d12cb40bad3cd55825
MacbookNikkiii <Nuttha.fu@hotmail.com>
2023-06-15 16:03:59 +0700

    edir package page

--- src/pages/Prices.jsx
+++ src/pages/Prices.jsx
@@ -1,34 +1,82 @@
 import Navbar from "../component/navbar";
+import packages from "../datas/prices.json";
+import Footer from "../component/footer";
+
 const PricePage = () => {
+  console.log(packages.PriveteSession);
   return (
     <>
       <Navbar />
       <div className="pricePageWraper">
         <div className="header">Choose Your Plan</div>
-        <div className="boxWrap">
-          {/* map here ?? */}
-          <PrizeBox />
-          <PrizeBox />
+        <h2 className="privateSessionHeader">Private Training</h2>
+        <div className="boxWrap PriveteSession">
+          {packages.PriveteSession?.map((p) => {
+            return (
+              <>
+                <PrizeBox
+                  amountSession={p.sessionsAmount}
+                  price={p.price}
+                  pricePerHour={p.pricePerSession}
+                  validsation={p.validation}
+                />
+              </>
+            );
+          })}
+        </div>
+        {/* Semi private */}
+        <h2 className="SemiPrivatex2Header">Semi Private Training x2</h2>
+        <div className="boxWrap SemiPrivatex2">
+          {packages.SemiPrivatex2?.map((p) => {
+            return (
+              <>
+                <PrizeBox
+                  amountSession={p.sessionsAmount}
+                  price={p.price}
+                  pricePerHour={p.pricePerSession}
+                  validsation={p.validation}
+                />
+              </>
+            );
+          })}
+        </div>
+        {/* x3 simi private */}
+        <h2 className="SemiPrivatex3Header">Semi Private Training x3</h2>
+        <div className="boxWrap SemiPrivatex3">
+          {packages.SemiPrivatex3?.map((p) => {
+            return (
+              <>
+                <PrizeBox
+                  amountSession={p.sessionsAmount}
+                  price={p.price}
+                  pricePerHour={p.pricePerSession}
+                  validsation={p.validation}
+                />
+              </>
+            );
+          })}
         </div>
       </div>
+      <Footer />
     </>
   );
 };
 
 export default PricePage;
 
-const PrizeBox = () => {
+const PrizeBox = ({ amountSession, price, pricePerHour, validsation }) => {
   return (
     <div className="contentBox">
       <div className="upper">
-        <div className="classType">Single session</div>
-        <div className="price">1800</div>
+        <div className="classType">{amountSession}</div>
+        <div className="price">{price}</div>
         <div className="dummyText">
-          Book your class and try out something new
+          {pricePerHour} / session
+          <p>**{validsation}** </p>
         </div>
       </div>
 
-      <button className="bookBtn">LET'S DO IT</button>
+      {/* <button className="bookBtn">LET'S DO IT</button> */}
     </div>
   );
 };
